6a8cb7afa385102d5e3734c7251820fdf7c111c9
Jonathan <jongschneider@gmail.com>
2017-04-07 16:45:50 -0400

    updated separated ContestPreview out from App.js and into its own component. Used a class in order to make each item clickable and the contents appear in the console.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'React';
 import Header from './Header';
-import ContestPreview from './ContestPreview';
+import ContestList from './ContestList';
 
 class App extends React.Component {
   state = {
@@ -15,16 +15,10 @@ class App extends React.Component {
   }
 
   render() {
-    debugger;
     return (
       <div className= 'App'>
         <Header message={ this.state.pageHeader } />
-        <div>
-          { this.state.contests.map( contest =>
-            <ContestPreview key={contest.id} {...contest} />
-          )}
-
-        </div>
+        <ContestList contests={ this.state.contests } />
       </div>
     );
   }
